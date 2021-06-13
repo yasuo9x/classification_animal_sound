287d1aa6f644d394f3f57b747a006c804a8b6051
yasuo9x <ahvana0@gmail.com>
2021-06-13 20:06:34 +0700

    update

--- Demo/check_1.py
+++ Demo/check_1.py
@@ -11,31 +11,44 @@ import librosa
 import librosa.display
 import IPython.display as ipd 
 
-# audio_fpath = ('../music/fileCut/Bò/')
-# audio_fpath = ('../music/fileCut/Ca sau/')
-# audio_fpath = ('../music/fileCut/Cho soi/')  
-# audio_fpath = ('../music/fileCut/Chuột/')
-# audio_fpath = ('../music/fileCut/Hổ/')
-# audio_fpath = ('../music/fileCut/Hươu/')
-# audio_fpath = ('../music/fileCut/Mèo/')
+# audio_fpath = ('../music/fileCut/Bo/')
+# audio_fpath = ('../music/fileCut/CaSau/')
+# audio_fpath = ('../music/fileCut/ChoSoi/')  
+# audio_fpath = ('../music/fileCut/Chuot/')
+# audio_fpath = ('../music/fileCut/Ho/')
+# audio_fpath = ('../music/fileCut/Huou/')
+# audio_fpath = ('../music/fileCut/Meo/')
 # audio_fpath = ('../music/fileCut/Ngựa/')
 # audio_fpath = ('../music/fileCut/Voi/')
-audio_fpath = ('../music/fileCut/Vượn/')
+# audio_fpath = ('../music/fileCut/Vuon/')
 
-audio_clips = os.listdir(audio_fpath)
-x = []
-for index in range(0,len(audio_clips)) :
-    tmp,sr = librosa.load(audio_fpath+audio_clips[index],44100)
-    print(audio_clips[index])
-    print(tmp.shape)
-    print(librosa.get_duration(tmp, sr))
-    x.append(tmp) 
-zcrs = []
-for index in x :
-    tmp = librosa.feature.zero_crossing_rate(index)[0]
-    print(tmp.shape)
-    tmp = tmp[:250]
-    zcrs.append(tmp)
+audio_fpath_x = '../music/fileCut/'
+audio_fpath_y = os.listdir(audio_fpath_x)
+
+for index in range(0,len(audio_fpath_y)) :
+    audio_fpath_z = os.listdir(audio_fpath_x + audio_fpath_y[index])
+    for index_x in range(0,len(audio_fpath_z)) :
+        tmp, sr = librosa.load(audio_fpath_x + audio_fpath_y[index] + '/'+ audio_fpath_z[index_x] , 44100)
+        print(tmp.shape)
+        print(librosa.get_duration(tmp, sr))
+
+
+
+
+# audio_clips = os.listdir(audio_fpath)
+# x = []
+# for index in range(0,len(audio_clips)) :
+#     tmp,sr = librosa.load(audio_fpath+audio_clips[index],44100)
+#     print(audio_clips[index])
+#     print(tmp.shape)
+#     print(librosa.get_duration(tmp, sr))
+#     x.append(tmp) 
+# zcrs = []
+# for index in x :
+#     tmp = librosa.feature.zero_crossing_rate(index)[0]
+#     print(tmp.shape)
+#     tmp = tmp[:250]
+#     zcrs.append(tmp)
     
-data = numpy.array(zcrs)
-print(data.shape)
+# data = numpy.array(zcrs)
+# print(data.shape)
--- Demo/test.py
+++ Demo/test.py
@@ -15,22 +15,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 FRAME_SIZE = 2048
 HOP_LENGTH = 512
-audio_fpath_1 = ('../music/fileCut/Cho soi/')  
+audio_fpath_1 = ('../music/fileCut/ChoSoi/')  
 audio_clips_1 = os.listdir(audio_fpath_1)
 
-audio_fpath_2 = ('../music/fileCut/Mèo/')
+audio_fpath_2 = ('../music/fileCut/Meo/')
 audio_clips_2 = os.listdir(audio_fpath_2)
 
-audio_fpath_3 = ('../music/fileCut/Bò/')
+audio_fpath_3 = ('../music/fileCut/Bo/')
 audio_clips_3 = os.listdir(audio_fpath_3)
 
 
 x_1 = [] # lay chuyen du lieu audio vao mang
 x_2 = []
 x_3 = []
-# print("No. of .wav files in audio folder = ",len(audio_clips_1))
-# print("No. of .wav files in audio folder = ",len(audio_clips_2))
-# print("No. of .wav files in audio folder = ",len(audio_clips_3))
+
 for index in range(0,len(audio_clips_1)) :
     tmp,sr = librosa.load(audio_fpath_1+audio_clips_1[index],44100)
     x_1.append(tmp)    
@@ -43,10 +41,6 @@ for index in range(0,len(audio_clips_3)) :
     tmp,sr = librosa.load(audio_fpath_3+audio_clips_3[index],44100)
     x_3.append(tmp)
 
-# print(len(x_1))
-# print(len(x_2))
-# print(sr) # mac dinh sr = 44100Hz sample rate
-
 zcrs_1 = [] # mang chua các phần tử của 1 video có zero crossing
 zcrs_2 = []
 zcrs_3 = []
@@ -54,21 +48,20 @@ for index in x_1 :
     tmp_1 = librosa.feature.zero_crossing_rate(index)[0]
     tmp_1 = tmp_1[:250]
     zcrs_1.append(tmp_1)
-# print(zcrs_1[0])
+
 for index in x_2 :
     tmp_2 = librosa.feature.zero_crossing_rate(index)[0]
     tmp_2 = tmp_2[:250]
     zcrs_2.append(tmp_2)
-#    zcrs.append(tmp_2)
+
 
 for index in x_3 :
     tmp_3 = librosa.feature.zero_crossing_rate(index)[0]
     tmp_3 = tmp_3[:250]
     zcrs_3.append(tmp_3)
-#    zcrs.append(tmp_3)
-# for index in zcrs :
-#     print(index.size)
-zcrs_x1 = numpy.array(zcrs_1[1:12])
+
+
+zcrs_x1 = numpy.array(zcrs_1)
 zcrs_x2 = numpy.array(zcrs_2)
 zcrs_x3 = numpy.array(zcrs_3)
 print(zcrs_x1.shape)
@@ -86,9 +79,9 @@ test = numpy.array([zcrs_1[0]])
 # target_data_2 = numpy.array([1] * 13)
 # target_data_3 = numpy.array([2] * 12)
 
-target_zcrs = numpy.array([0] * 11 + [1] * 12 + [2] * 11)
+target_zcrs = numpy.array([0] * 13 + [1] * 14 + [2] * 12)
 
-clf = neighbors.KNeighborsClassifier(n_neighbors=10,p=2) #
+clf = neighbors.KNeighborsClassifier(n_neighbors=10,p=2,weights='distance')
 clf.fit(zcrs,target_zcrs)
 y = clf.kneighbors(test)
 y_pred = clf.predict(test) # du doan lay nhan cua data moi
